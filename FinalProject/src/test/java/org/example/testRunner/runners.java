92ae65f394ffd16de9ce4f384fe37dcb166698b5
Nada0Emad <47033540+Nada0Emad@users.noreply.github.com>
2023-01-30 12:28:47 +0200

    Updated version

--- FinalProject/src/test/java/org/example/testRunner/runners.java
+++ FinalProject/src/test/java/org/example/testRunner/runners.java
@@ -1,9 +1,11 @@
 package org.example.testRunner;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
 
-
+@RunWith(Cucumber.class)
 @CucumberOptions(
 features = "src/main/resources/features",
 glue = {"org.example.stepDefs"},
@@ -13,6 +15,5 @@ plugin = {"pretty",
         "junit:target/cukes.xml",
         "return:target/rerun.txt"},
         tags = "@Smoke")
-
 public class runners extends AbstractTestNGCucumberTests {
 }
